8e97188dbecd3e7b7700b1db58a3b1ac941fa0a4
Andrew C. Scoppa <andrew-scoppa@github.com>
2024-06-17 07:26:00 -0400

    update

--- .github/scripts/sample.sh
+++
@@ -1,7 +0,0 @@
-if [ "$message" ]; then
-    echo $message
-    echo $repo_owner
-    gh issue create --title "$message" --body $repo_owner" requests comments on the message..."  
-else
-    echo "A welcome message was not provided."
-fi
